b63b544ba69d15cbccd1e950d26a16e8220913a2
Mark Ruys <mark.ruys@in2sports.net>
2017-01-05 10:22:05 +0100

    Drop Python 2 support

--- setup.py
+++ setup.py
@@ -52,12 +52,8 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'Topic :: System :: Monitoring',
 
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
