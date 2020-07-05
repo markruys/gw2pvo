a0c814001ab87b210cf8c0e1e922cc1b5c4e50b8
Mark Ruys <mark.ruys@sensoterra.com>
2020-07-05 17:26:06 +0200

    Add oauthlib and requests-oauthlib dependencies

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['astral>=2', 'requests'],
+    install_requires=['astral>=2', 'requests', 'oauthlib', 'requests-oauthlib'],
 
     # Astral >=2 requires Python>=3.6
     python_requires='>=3.6',
